96b7aef869a3bd5bd893a3fad53ffd0ed9e44771
Erica Ehrhardt <eee@thumbtack.com>
2018-03-19 18:34:37 -0700

    Fix copyright dates touched in this change

--- test/com/thumbtack/becquerel/controllers/HomeControllerTest.scala
+++ test/com/thumbtack/becquerel/controllers/HomeControllerTest.scala
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2017 Thumbtack
+ *    Copyright 2017–2018 Thumbtack
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
